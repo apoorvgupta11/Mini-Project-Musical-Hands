9d986d9d84f9a0f08bcabc9d3a9eec274a02059e
apoorvgupta25 <apurvagupta45@gmail.com>
2020-09-17 09:37:43 +0530

    Seller Signup

---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php
+// setcookie("name", "", time() - 3600);
+// setcookie("username", "", time() - 3600);
+// setcookie("location", "", time() - 3600);
+
+setcookie('name', null, -1, '/');
+setcookie('username', null, -1, '/');
+setcookie('location', null, -1, '/');
+
+
+echo'<script>alert("cookie deleted Successfully")</script>';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <p>Cookies are deleted</p>
+
+        <form action="signup_seller.php">
+            <input type="submit" value="Signup Seller" />
+        </form>
+    </body>
+</html>
