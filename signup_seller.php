cf6b2e0f56af5643d4e92d0db56a5740864603aa
apoorvgupta25 <apurvagupta45@gmail.com>
2020-09-17 09:37:06 +0530

    Seller Signup

--- signup_seller.php
+++ signup_seller.php
@@ -3,21 +3,26 @@
     <head>
         <style type="text/css">
 
+            /* cookie - set the cookie
+            login - display cookie value and logout button
+            logout - destroy cookie */
+
             html, body {
                 /* height: 100%; */
-                /* background-color: blue;
-                background-image: linear-gradient(to right, #21007E, #E51EB6); */
+                /* background-color: #F9F8F9; */
+                /* background-color: #E5F9F7; */
+                background-color: #F9F9F9;
+                /*background-image: linear-gradient(to right, #21007E, #E51EB6); */
             }
 
             .avatar{
-                width:100px;
+                width:80px;
                 position: absolute;
                 top: -50px;
                 left: calc(50% - 50px);
             }
 
             .error {color: #FF0000;}
-
 
             .form-inline input {
               margin: 5px 10px 5px 0;
@@ -33,7 +38,8 @@
 
             .form-inline input[type="submit"]{
                 width :85%;
-                background-color: coral;
+                margin-top: 20px;
+                background-color: #FF5722;
                 color: white;
                 font-size: 15px;
             }
@@ -48,8 +54,13 @@
             }
 
             .form-outline{
-                border-style: outset;
-                padding: 50px 0px 10px 50px;
+                /* background-color: #F4F4F4; */
+                /* background-color: #D2D6DE; */
+                background-color: #fff;
+                border: 3px solid #eee;
+                box-shadow: 1px 1px 4px #eee;
+                /* border-style: outset; */
+                padding: 40px 0px 0px 50px;
                 width: 400px;
                 height: auto;
                 border-radius: 5px;
@@ -59,8 +70,6 @@
                 transform: translate(-50%,-50%);
             }
 
-
-
         </style>
     </head>
 
@@ -68,34 +77,40 @@
         $name = "";
         $password = "";
         $cnf_password = "";
+        $gstin = "";
         $username = "";
         $nameErr = "";
         $usernameErr = "";
         $passwordErr = "";
         $cnf_passwordErr = "";
         $locationErr = "";
+        $gstinErr = "";
+
+        $passwordErrShow = "";
+        $cnf_passwordErrShow = "";
+
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Username
             if (empty($_POST["username"]))
-              $usernameErr = "Username is required";
+              $usernameErr = "* Username is required";
             else
               $username = test_input($_POST["username"]);
 
             // Password
             if (empty($_POST["password"]))
-              $passwordErr = "Password is required";
+              $passwordErr = "* Password is required";
             else
               $password = test_input($_POST["password"]);
 
             if (empty($_POST["cnf_password"]))
-                $cnf_passwordErr = "Password is required";
+                $cnf_passwordErr = "* Password is required";
             else
                 $cnf_password = test_input($_POST["cnf_password"]);
 
             // Name
             if (empty($_POST["name"]))
-                $nameErr = "Name is required";
+                $nameErr = "* Name is required";
             else
                 $name = test_input($_POST["name"]);
 
@@ -103,7 +118,14 @@
             $location = $_POST['locations'];
 
             if ($location == "Empty")
-                $locationErr = "Select Correct Location";
+                $locationErr = "* Select Correct Location";
+
+            //GSTIN
+            if (empty($_POST["gstin"]))
+                $gstinErr = "* GSTIN Number is required";
+            else
+                $gstin = test_input($_POST["gstin"]);
+
         }
 
 
@@ -113,26 +135,65 @@
           $data = htmlspecialchars($data);
           return $data;
         }
-     ?>
 
+
+        if (isset($_POST['submit'])) {
+
+            // Validate password
+            $uppercase = preg_match('@[A-Z]@', $password);
+            $lowercase = preg_match('@[a-z]@', $password);
+            $number    = preg_match('@[0-9]@', $password);
+            $specialChars = preg_match('@[^\w]@', $password);
+
+            if (strlen($password) < 8)
+                $passwordErr = "* Password should be at least 8 characters in length";
+            elseif (!$uppercase)
+                $passwordErr = "* Password should include at least one upper case letter";
+            elseif (!$lowercase)
+                $passwordErr = "* Password should include at least one lower case letter";
+            elseif (!$number)
+                $passwordErr = "* Password should include at least one number";
+            elseif (!$specialChars)
+                $passwordErr = "* Password should include at least one special character";
+            elseif (strcmp($cnf_password, $password)!=0)
+                $cnf_passwordErr = "* Passwords do not match";
+            elseif (strcmp($cnf_password, $password)==0)
+                $passwordErr = "";
+
+            // GST
+            $pattern = "/[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[0-9]{1}[A-Z]{1}[0-9]{1}/";
+            $check = preg_match($pattern, $gstin);
+
+            if (!$check)
+                $gstinErr = "* Enter Valid GST Number";
+
+            if(!filter_var($username, FILTER_VALIDATE_EMAIL))
+                $usernameErr = "* Invalid Username";
+
+
+            // redirect to new page
+            if ($passwordErr=="" && $cnf_passwordErr=="" && $gstinErr=="" && $usernameErr=="" && $locationErr=="" && $nameErr==""){
+                header('Location: http://localhost/WP2/Mini%20Project/cookie.php?name='.$name.'&username='.$username.'&location='.$location);
+                exit();
+            }
+        }
+     ?>
 
     <body>
         <h1 align="center">Musical Hands</h1>
-        <!-- <p>Begin Your Musical Journey From Here</p> -->
-        <!-- <br><br> -->
         <div class="form-outline">
             <img src="avatar.png" class="avatar">
             <form method="post" class="form-inline" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                Company/Business name <span class="error"> * <?php echo $nameErr;?> </span> <br> <input type="text" name="name" value="<?php echo $name;?>"><br><br>
+                Company/Business name <span class="error">  <?php echo $nameErr;?> </span> <br> <input type="text" name="name" value="<?php echo $name;?>"><br><br>
 
-                Email Id <span class="error"> *  <?php echo $usernameErr;?> </span> <br> <input type="text" name="username" value="<?php echo $username;?>"><br><br>
+                Email Id <span class="error"><?php echo $usernameErr;?> </span> <br> <input type="text" name="username" value="<?php echo $username;?>"><br><br>
 
-                Password <span class="error"> * <?php echo $passwordErr;?> </span> <br> <input type="password" name="password" id="pass" value="<?php echo $password;?>">
+                Password <span class="error"><?php echo $passwordErr;?> </span> <br> <input type="password" name="password" id="pass" value="<?php echo $password;?>">
                 <img src="eye1.png" alt="" id="eye_img" height="20px" onclick="myFunction()">
                 <br><br>
-                Confirm Password <span class="error"> * <?php echo $cnf_passwordErr;?> </span> <br> <input type="password" name="cnf_password" value="<?php echo $cnf_password;?>"><br><br>
+                Confirm Password <span class="error"><?php echo $cnf_passwordErr;?> </span> <br> <input type="password" name="cnf_password" value="<?php echo $cnf_password;?>"><br><br>
 
-                Your Location <span class="error"> * <?php echo $locationErr;?> </span> <br>
+                Your Location <span class="error"><?php echo $locationErr;?> </span> <br>
                 <select name="locations">
                   <option value="Empty"> --- Select Location ---</option>
                   <option value="Mumbai">Mumbai</option>
@@ -143,10 +204,10 @@
                   <option value="Kolkata">Kolkata</option>
                 </select> <br><br>
 
-                PAN Card Number <span class="error"> * </span> <br> <input type="text" name="" value="">
-
+                GSTIN Number <span class="error">  <?php echo $gstinErr;?> </span> <br> <input type="text" name="gstin" value="<?php echo $gstin;?>">
 
                 <input type="submit" name="submit" value="Sign Up">
+
                 <br><br>
             </form>
         </div>
@@ -155,58 +216,19 @@
         <script>
             function myFunction() {
               var x = document.getElementById("pass");
-              if (x.type === "password") {
+              if (x.type === "password")
                 x.type = "text";
-              } else {
+              else
                 x.type = "password";
-              }
+
 
               var img = document.getElementById("eye_img");
-              if (img.getAttribute('src') == "eye1.png") {
+              if (img.getAttribute('src') == "eye1.png")
                 img.src = "eye2.png";
-              } else {
+              else
                 img.src = "eye1.png";
-              }
+
             }
         </script>
     </body>
 </html>
-
-
-<?php
-    if (isset($_POST['submit'])) {
-        // $password = $_POST["password"];
-        // $username = $_POST["username"];
-
-        // Validate password strength
-        $uppercase = preg_match('@[A-Z]@', $password);
-        $lowercase = preg_match('@[a-z]@', $password);
-        $number    = preg_match('@[0-9]@', $password);
-        $specialChars = preg_match('@[^\w]@', $password);
-
-        // if ($nameErr != "")
-        //     echo "<p style='color:red'>".$nameErr."</p>";
-
-        if (strlen($password) < 8)
-            $passwordErr = "Password should be at least 8 characters in length";
-        elseif (!$uppercase)
-            $passwordErr = "Password should include at least one upper case letter";
-        elseif (!$lowercase)
-            $passwordErr = "Password should include at least one lower case letter";
-        elseif (!$number)
-            $passwordErr = "Password should include at least one number";
-        elseif (!$specialChars)
-            $passwordErr = "Password should include at least one special character";
-        elseif (strcmp($cnf_password, $password)!=0)
-            $cnf_passwordErr = "Passwords do not match";
-        elseif (strcmp($cnf_password, $password)==0)
-            $passwordErr = "Strong password";
-
-
-        $location = $_POST['locations'];
-
-        if ($location == "Empty")
-            $locationErr = "Select Correct Location";
-
-    }
- ?>
